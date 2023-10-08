a7c1521be2252b6c8affc2eb0563f91ac737bab0
Paulo de Tarso <potassio@hotmail.com>
2023-10-08 12:22:43 -0300

    Configuração de autenticação na classe Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Auth_Bearer_JWT
 {
     public class Program
@@ -10,14 +15,33 @@ namespace Auth_Bearer_JWT
 
             builder.Services.AddControllers();
 
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
