9e041c7a072a221d22b8b132e3b21421b3e519d9
Paulo de Tarso <potassio@hotmail.com>
2023-10-08 11:53:16 -0300

    Criação da model de usuários e do repositório em memória

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth_Bearer_JWT.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using Auth_Bearer_JWT.Models;
+
+namespace Auth_Bearer_JWT.Repositories
+{
+    public class UserRepository
+    {
+        public static User? Get(string name, string password)
+        {
+            List<User> users = new()
+            {
+                new User()
+                {
+                    UserId = 1,
+                    Name = "Batman",
+                    Password = "batman123",
+                    Role = "boss"
+                },
+
+                new User()
+                {
+                    UserId = 2,
+                    Name = "Robin",
+                    Password = "robin123",
+                    Role = "employee"
+                },
+
+                new User()
+                {
+                    UserId = 3,
+                    Name = "Coringa",
+                    Password = "coringa123",
+                    Role = "blocked"
+                }
+            };
+
+            return users.FirstOrDefault(x => 
+                                        x.Name.ToLower() == name.ToLower() && 
+                                        x.Password.ToLower() == password.ToLower());
+        }
+    }
+}
