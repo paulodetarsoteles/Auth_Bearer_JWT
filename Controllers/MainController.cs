219984358b2c9b347a64f72cd11c2dad5eb84105
Paulo de Tarso <potassio@hotmail.com>
2023-10-08 11:23:58 -0300

    Add project files.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth_Bearer_JWT.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        public IActionResult Index() => Ok("Api Running...");
+    }
+}
