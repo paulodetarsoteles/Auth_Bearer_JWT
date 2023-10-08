8f47a4d8b5b388682bf05fe2dc150ff906d99c80
Paulo de Tarso <potassio@hotmail.com>
2023-10-08 17:55:58 -0300

    Implantação do EndPoint de login

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Auth_Bearer_JWT.Models;
+using Auth_Bearer_JWT.Repositories;
+using Auth_Bearer_JWT.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Auth_Bearer_JWT.Controllers
 {
@@ -7,5 +10,20 @@ namespace Auth_Bearer_JWT.Controllers
     public class MainController : ControllerBase
     {
         public IActionResult Index() => Ok("Api Running...");
+
+        [HttpPost("login")]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            User? user = UserRepository.Get(model.Name, model.Password);
+
+            if (user is null)
+                return NotFound("Usuário ou senha inválidos");
+
+            string token = TokenService.GenerateToken(user);
+
+            user.Password = "****";
+
+            return Ok(new { user = user, token = token });
+        }
     }
 }
